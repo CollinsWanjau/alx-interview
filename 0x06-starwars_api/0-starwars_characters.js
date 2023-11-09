252a111b13282c2e05e0802d6482d2b653458c6a
colloso999 <tripperskripper@gmail.com>
2023-11-09 22:51:11 +0300

    StarWars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,29 @@
+#!/usr/bin/node
+
 const request = require('request');
 
 const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-if (!movieId) {
-  console.error('Please provide a movie ID as a command-line argument.');
-  process.exit(1);
-}
-
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-    process.exit(1);
-  }
-
-  const movie = JSON.parse(body);
-
-  console.log(`Characters in ${movie.title}:`);
-
-  movie.characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error(`Error: ${charResponse.statusCode} - ${charResponse.statusMessage}`);
-        return;
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-    });
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
   });
+}
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+    sendRequest(characterList, 0);
+  }
 });
