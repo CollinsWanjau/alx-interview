d1683ffb899ec51fa1ee81d24685cacd06c8f5d0
colloso999 <tripperskripper@gmail.com>
2023-11-16 18:54:12 +0300

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,8 +5,8 @@
 
 def transpose_matrix(matrix, n):
     """ Transpose a matrix"""
-    for i in range(len(matrix)):
-        for j in range(i):
+    for i in range(n):
+        for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
 def reverse_matrix(matrix):
