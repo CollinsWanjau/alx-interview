ab0237bfffdf3548fdcec2af1ef40d37d928d2f1
colloso999 <tripperskripper@gmail.com>
2023-11-16 18:51:47 +0300

    Rotate 2D Matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,24 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def transpose_matrix(matrix, n):
+    """ Transpose a matrix"""
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+def reverse_matrix(matrix):
+    """ Reverse a matrix """
+    for row in matrix:
+        row.reverse()
+
 def rotate_2d_matrix(matrix):
-    """
-    Do not return anything, modify matrix in-place instead.
-    """
+    """ Rotate the 2d matrix"""
     n = len(matrix)
-    # Transposing the Matrix
-    for i in range(0, n):
-        for j in range(i, n):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[j][i]
-            matrix[j][i] = temp
-        # shifting the elements accodingly...
-    for i in range(0, n):
-        for j in range(0, n//2):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[i][n-1-j]
-            matrix[i][n-j-1] = temp
+    print(n)
+    transpose_matrix(matrix, n)
+    reverse_matrix(matrix)
+    return matrix
+    
